f2da522424303f22181161454c8dcb5af8e4dff3
Deepakvikrant <100572841+Deepakvikrant@users.noreply.github.com>
2022-09-12 09:54:53 +0530

    page components added

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>this is my About component</div>
+  )
+}
+
+export default About
---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>
+        <h1>This is Blog home page </h1>
+    </div>
+  )
+}
+
+export default blog
