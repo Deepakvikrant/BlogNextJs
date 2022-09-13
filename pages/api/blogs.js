06b0be90fd03a02ee34175fe9cdf4e0c33a24b9a
Deepakvikrant <100572841+Deepakvikrant@users.noreply.github.com>
2022-09-13 13:14:20 +0530

    blog post added to front end

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,10 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     //console.log(req,res);
-    fs.readdir("blogdata",(err,data)=>{
-        console.log(data);
-        res.status(200).json(data)
-    })
-  }
+    // fs.readdir("blogdata",(err,data)=>{
+    //     console.log(data);
+
+    //     let allBlogs = [];
+
+    //     //for read each item in data
+    //     data.forEach((item)=>{
+    //         console.log(item);
+    //         fs.readFile(('blogdata' + item),(d)=>{
+    //             allBlogs.push(d)
+    //         })
+    //     })
+    //     res.status(200).json(allBlogs)
+    // })
+
+    let data = await fs.promises.readdir("blogdata")
+    let myfile;
+    let allBlogs = [];
+
+    for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+        console.log(item);
+        myfile = await fs.promises.readFile(("blogdata/" + item), 'utf-8')
+        //console.log(myfile);
+        allBlogs.push(JSON.parse(myfile))
+
+    }
+    res.status(200).json(allBlogs)
+}
--- pages/blog.js
+++ pages/blog.js
@@ -1,34 +1,45 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from "react";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 // Step 1 : Collect all the files from blogdata directory
 
-
 const blog = () => {
+  const [blogs, setBlogs] = useState([]);
 
+  useEffect(() => {
+    console.log("use effect is runing");
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        console.log(parsed);
+        setBlogs(parsed);
+      });
+  }, []);
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div>
-          <Link href={'/blogpost/learn-javascript'}>
-            <h2 className={styles.blogItemh3}>How to learn JavaScript in 2022?</h2></Link>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
-        <div className="blogItem">
-          <h2>How to learn JavaScript in 2022?</h2>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
-        <div className="blogItem">
-          <h2>How to learn JavaScript in 2022?</h2>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
+        <h1>Latest Blog Post</h1>
+        {blogs.map((blogItem) => {
+          return (
+            <div key={blogItem.slug}>
+              <Link href={`/blogpost/${blogItem.slug}`}>
+                <h2 className={styles.blogItemh3}>{blogItem.title}</h2>
+              </Link>
+              <p>{blogItem.content.substr(0,400)}</p>
+            </div>
+          );
+        })}
+
+
       </main>
     </div>
-  )
+  );
 };
 
-
-export default blog
+export default blog;
